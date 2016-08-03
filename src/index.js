ccf467176582192a75446a6b7298c139c810248e
Brian Tanner <brian@tanner.io>
2016-08-03 16:21:30 +0700

    remove streaming code, add dist to path

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
+const path = require('path');
+const os = require('os');
+
+process.env.PATH = path.resolve(path.join('.', 'dist', os.platform(), os.arch())) + path.delimiter + process.env.PATH;
+
+console.log(process.env.PATH)
+
 require('./app/main');
