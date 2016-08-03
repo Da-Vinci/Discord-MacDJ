4083c7c792a703237802e07302e974db1233524f
Brian Tanner <brian@tanner.io>
2016-08-03 23:24:57 +0700

    moved to fork of discordie with ffmpeg-static

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const path = require('path');
+// const path = require('path');
 const os = require('os');
 const electron = require('electron');
 const app = electron.app;
@@ -10,6 +10,6 @@ var version = app.getVersion();
 
 console.log(platform, version);
 
-process.env.PATH = path.resolve(path.join('.', 'dist', os.platform(), os.arch())) + path.delimiter + process.env.PATH;
+// process.env.PATH = path.resolve(path.join('.', 'dist', os.platform(), os.arch())) + path.delimiter + process.env.PATH;
 
 require('./app/main');
