99eeebe82978e8f63543e5f864848b47a837ce7d
Brian Tanner <brian@tanner.io>
2016-08-05 22:27:10 +0700

    move credits to module

---
+++ src/app/credits.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const credits = [
+  {
+    title: 'Bot',
+    list: [
+      { name: 'Website', type: 'link', href: 'http://macdj.tk' },
+      { name: 'Discord', type: 'link', href: 'http://macdj.tk/discord' }
+    ]
+  },
+  {
+    title: 'Developers',
+    list: [
+      { name: 'NoobLance™#3500', type: 'human' },
+      { name: 'Gus#0291', type: 'human' },
+      { name: '```Macdja38#7770'.replace(/`/g, String.fromCharCode(8203) + '`'), type: 'human' }
+    ]
+  },
+  {
+    title: 'Contributors',
+    list: [
+      { name: 'Dean#5368', type: 'link', href: 'http://discord.fm' },
+      { name: 'Gexo#6439', type: 'human' },
+      { name: 'Lucky_Evie#4611', type: 'human' },
+      { name: 'Meowbeast™#4695', type: 'human' }
+    ]
+  },
+  {
+    title: "Accomplishments",
+    list: [
+      { name: 'PvPCraft', type: 'link', href: 'http://pvpcraft.ca' },
+      { name: 'Dyno', type: 'link', href: 'https://www.dynobot.net' }
+    ]
+  }
+];
+
+module.exports = credits;
--- src/app/main.js
+++ src/app/main.js
@@ -11,42 +11,10 @@ const Menu = electron.Menu;
 const appMenu = require('./appMenu')(app);
 const utils = require('./lib/utils');
 const Player = require('./lib/player');
+const credits = require('./credits');
 const client = new Discord();
 const dbPath = path.join(app.getPath('userData'), 'config.db');
 
-const credits = [
-  {
-    title: 'Bot',
-    list: [
-      { name: 'Website', type: 'link', href: 'http://macdj.tk' },
-      { name: 'Discord', type: 'link', href: 'http://macdj.tk/discord' }
-    ]
-  },
-  {
-    title: 'Developers',
-    list: [
-      { name: 'NoobLance™#3500', type: 'human' },
-      { name: 'Gus#0291', type: 'human' },
-      { name: '```Macdja38#7770'.replace(/`/g, String.fromCharCode(8203) + '`'), type: 'human' }
-    ]
-  },
-  {
-    title: 'Contributors',
-    list: [
-      { name: 'Dean#5368', type: 'link', href: 'http://discord.fm' },
-      { name: 'Gexo#6439', type: 'human' },
-      { name: 'Lucky_Evie#4611', type: 'human' },
-      { name: 'Meowbeast™#4695', type: 'human' }
-    ]
-  },
-  {
-    title: "Accomplishments",
-    list: [
-      { name: 'PvPCraft', type: 'link', href: 'http://pvpcraft.ca' },
-      { name: 'Dyno', type: 'link', href: 'https://www.dynobot.net' }
-    ]
-  }
-];
 
 let main;
 
